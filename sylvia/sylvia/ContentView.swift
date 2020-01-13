58746b543f84142b446341ff3e6f1997386bf815
Dan Davison <dandavison7@gmail.com>
2020-01-12 22:16:32 -0500

    Add iOS target

---
+++ sylvia/sylvia/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  sylvia
+//
+//  Created by Dan Davison on 1/12/20.
+//  Copyright © 2020 Dan Davison. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
