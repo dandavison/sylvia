d72f8610faf2fb87b215538f3dbac1997e287d53
Dan Davison <dandavison7@gmail.com>
2020-01-14 12:40:06 -0500

    Audio recorder app IV
    https://www.blckbirds.com/post/voice-recorder-app-in-swiftui-2

--- sylvia/sylvia/RecordingsList.swift
+++ sylvia/sylvia/RecordingsList.swift
@@ -12,19 +12,36 @@ import SwiftUI
 struct RecordingRow: View {
 
     var audioURL: URL
-    
+
+    @ObservedObject var audioPlayer = AudioPlayer()
+
     var body: some View {
         HStack {
             Text("\(audioURL.lastPathComponent)")
             Spacer()
+            if audioPlayer.isPlaying == false {
+                Button(action: {
+                    self.audioPlayer.startPlayback(audio: self.audioURL)
+                }) {
+                    Image(systemName: "play.circle")
+                        .imageScale(.large)
+                }
+            } else {
+                Button(action: {
+                    self.audioPlayer.stopPlayback()
+                }) {
+                    Image(systemName: "stop.fill")
+                        .imageScale(.large)
+                }
+            }
         }
     }
 }
 
 struct RecordingsList: View {
-    
+
     @ObservedObject var audioRecorder: AudioRecorder
-    
+
     var body: some View {
         List {
             ForEach(audioRecorder.recordings, id: \.createdAt) { recording in
